af1091690b7bbac9b8728209d457b78e2c0b7925
Pedro <pedroh1510@hotmail.com>
2020-05-05 12:16:54 -0300

    feat: add modelo Tools
    Adicionado modelo Tools para interação com a tabela de ferramentas

---
+++ src/app/models/Tools.js
@@ -0,0 +1,26 @@
+import mongo from "../../database";
+
+const tag = {
+  type: String,
+  required: true
+}
+
+const ToolSchema = new mongo.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  tags: [tag]
+})
+
+const Tool = mongo.model("Tool", ToolSchema)
+
+export default Tool
