9f9859961a163882475b5e919000c19276888fc0
Pedro <pedroh1510@hotmail.com>
2020-05-15 00:15:53 -0300

    fix: retorno de mensagem de erro na validação
    Inserido mensagens de erro do celebrate

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import './config/dotenv'
+import { errors } from 'celebrate'
 import express from 'express'
 
 import routes from './routes'
@@ -8,5 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use(errors())
 
 export default app
