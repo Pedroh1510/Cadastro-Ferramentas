a21483132507692d6cce77ac4cd957d9add05b91
Pedro <pedroh1510@hotmail.com>
2020-07-17 16:43:12 -0300

    fix: run MongoDB memory server for tests

---
+++ jest-mongodb.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    instance: {
+      dbName: 'test'
+    },
+    binary: {
+      version: '4.0.3',
+      skipMD5: true
+    },
+    instance: {},
+    autoStart: true
+  }
+};
