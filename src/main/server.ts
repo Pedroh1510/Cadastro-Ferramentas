5f02a79a907b8e5aa0c578aa01fd1ca3ab4ac968
Pedro <pedroh1510@hotmail.com>
2020-08-03 00:25:58 -0300

    fix: servidor iniciando de forma correta

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,9 @@
 import { typeOrmHelper } from '../infra/db/typeOrm/typeOrmHelper/typeOrmHelper'
 import app from './config/app'
 import env from './config/env'
+import { makeConfigConnection } from './factories/db/connection'
 
-typeOrmHelper.connect()
+typeOrmHelper.connect(makeConfigConnection(env.configConnection))
   .then(async () => {
     app.listen(env.port, () => console.log(`Server running http://localhost:${env.port}`))
   })
